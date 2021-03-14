7d48b28956a81c5271163b89110a176006288306
IsaacRh <isaac.roj_12@hotmail.com>
2021-03-14 10:46:17 -0600

    [UPDATE] -- update config file for database connection

--- Vehicle-Fleet-Model/app/__init__.py
+++ Vehicle-Fleet-Model/app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 def create_app():
     app = Flask(__name__)
 
     # set config to app
     app.config.from_object(Config)
+    db = SQLAlchemy()
+    db.init_app(app)
     return app
--- Vehicle-Fleet-Model/app/config.py
+++ Vehicle-Fleet-Model/app/config.py
@@ -1,4 +1,8 @@
+user_bd = 'root'
+password_db = 'root'
+host_db = 'localhost'
+name_bd = 'vehicleFleet'
 class Config:
     SECRET_KEY = 'secret'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/miniblog'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(user_bd, password_db, host_db, name_bd)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
