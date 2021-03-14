b53548d014d1cd45551480c44fad19d0a1174788
IsaacRh <isaac.roj_12@hotmail.com>
2021-03-14 12:48:09 -0600

    [ADD/UPDATE] -- requirement file update and add migrations to models

--- Vehicle-Fleet-Model/app/__init__.py
+++ Vehicle-Fleet-Model/app/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from .config import Config
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from flask_migrate import Migrate
+from .models import db
 
+# CHECK the database in server, if not found, its created
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 if not database_exists(engine.url):
     create_database(engine.url)
 
+migrate = Migrate()
 def create_app():
     app = Flask(__name__)
-
     # set config to app
     app.config.from_object(Config)
-    db = SQLAlchemy()
     db.init_app(app)
+    migrate.init_app(app, db)
     return app
