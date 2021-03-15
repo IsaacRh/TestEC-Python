df2ee20a9b6aecb92667562c1c81e21170daef8a
IsaacRh <isaac.roj_12@hotmail.com>
2021-03-15 09:33:29 -0600

    [FIX/UPDATE] update test and fix function in vowels file

--- tests_vowel.py
+++ tests_vowel.py
@@ -12,6 +12,11 @@ class TestVowel(unittest.TestCase):
         text = "Hola MundO dEsdE PyThOn"
         result = vowels.set_text(text)
         self.assertEqual(result[1], 7)
+    
+    def test_accented_vowels(self):
+        text = "esté es un texto de prueba"
+        result = vowels.set_text(text)
+        self.assertEqual(result[0], "istí is an tixtu di praibe")
 
 if __name__ == '__main__':
     unittest.main()
--- vowels.py
+++ vowels.py
@@ -1,6 +1,9 @@
 # set array vowels
 vowels_lowercase = ['a', 'e', 'i', 'o', 'u']
 vowels_uppercase = ['A', 'E', 'I', 'O', 'U']
+accented_vowels_lowercase = ['á', 'é', 'í', 'ó', 'ú']
+accented_vowels_uppercase = ['Á', 'É', 'Í', 'Ó', 'Ú']
+
 
 def get_next_vowel(check_vowel, vowels_array):
     index = 0
@@ -14,14 +17,18 @@ def set_text(text):
     new_text = ''
     vowel_counter = 0
     for character in text:
-        if character in vowels_lowercase:
-            next_vowel = get_next_vowel(character, vowels_lowercase)
+        if (character in vowels_lowercase) or (character in vowels_uppercase) or (character in accented_vowels_lowercase) or (character in accented_vowels_uppercase):
+            vowels_array = []
+            if character in vowels_lowercase: 
+                vowels_array = vowels_lowercase
+            elif character in vowels_uppercase : vowels_array = vowels_uppercase
+            elif character in accented_vowels_lowercase : vowels_array = accented_vowels_lowercase
+            elif character in accented_vowels_uppercase : vowels_array = accented_vowels_uppercase
+            
+            next_vowel = get_next_vowel(character, vowels_array)
             new_text += next_vowel
             vowel_counter += 1
-        elif character in vowels_uppercase:
-            next_vowel = get_next_vowel(character, vowels_uppercase)
-            new_text += next_vowel
-            vowel_counter += 1
+
         else:
             new_text += character
 
