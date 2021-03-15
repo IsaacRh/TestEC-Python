6af4606544670ae15e946d32eb3ac1e9ba0df6ca
IsaacRh <isaac.roj_12@hotmail.com>
2021-03-15 09:59:00 -0600

    [FIX] -- execute flake8 and fix code

--- vowels.py
+++ vowels.py
@@ -10,8 +10,10 @@ def get_next_vowel(check_vowel, vowels_array):
     for vowel in vowels_array:
         index += 1
         if vowel == check_vowel:
-            if index > len(vowels_array)-1: return vowels_array[0]
+            if index > len(vowels_array)-1:
+                return vowels_array[0]
             return vowels_array[index]
+
 
 def set_text(text):
     new_text = ''
@@ -21,10 +23,12 @@ def set_text(text):
             vowels_array = []
             if character in vowels_lowercase: 
                 vowels_array = vowels_lowercase
-            elif character in vowels_uppercase : vowels_array = vowels_uppercase
-            elif character in accented_vowels_lowercase : vowels_array = accented_vowels_lowercase
-            elif character in accented_vowels_uppercase : vowels_array = accented_vowels_uppercase
-            
+            elif character in vowels_uppercase:
+                vowels_array = vowels_uppercase
+            elif character in accented_vowels_lowercase:
+                vowels_array = accented_vowels_lowercase
+            elif character in accented_vowels_uppercase:
+                vowels_array = accented_vowels_uppercase
             next_vowel = get_next_vowel(character, vowels_array)
             new_text += next_vowel
             vowel_counter += 1
@@ -33,6 +37,7 @@ def set_text(text):
             new_text += character
 
     return (new_text, vowel_counter)
+
 
 if __name__ == '__main__':
     # get data from input
